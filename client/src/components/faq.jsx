7c49780ef616adea73870d66dfa21c4985a40aa6
Hammad Raza <01hammadraza@gmail.com>
2026-02-01 16:41:18 +0500

    auto deploy

--- client/src/components/faq.jsx
+++ client/src/components/faq.jsx
@@ -328,7 +328,6 @@ function Faq() {
         
         </div> */}
 
-        {/* Mobile Quick Actions */}
         {isMobile && (
           <div className="fixed bottom-4 left-4 right-4 bg-white rounded-xl shadow-lg 
             p-4 border border-gray-200 z-10">
